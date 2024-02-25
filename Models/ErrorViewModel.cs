98b3ff1742acc103fc145fe811290bffc06a5bd4
Akhil-Dobariya <akhil.dobariya@outlook.com>
2024-02-25 15:47:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BileshwarOilMill.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
